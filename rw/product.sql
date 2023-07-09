abd6a10a32400b9b1bfffb0c2d2c967a91344c29
Florian Klein <florian.klein@free.fr>
2023-07-09 22:47:21 +0200

    fixes

--- rw/product.sql
+++ rw/product.sql
@@ -1,4 +1,5 @@
 create schema pim1;
+
 create table pim1.product (
   uuid bytea,
   family_id int,
@@ -24,28 +25,32 @@ create table pim1.product (
 );
 
 drop sink if exists product_stats;
-drop materialized view pim1.product_value;
+drop materialized view if exists pim1.product_value;
 
 create materialized view pim1.product_value as
-  with by_raw_value (product_id, family, rest) as (
-    select id, family_id, jsonb_each(raw_values) from pim1.product
+  with by_raw_value (product_id, rest) as (
+    select id, jsonb_each(raw_values) from pim1.product
   ),
-  by_attr (product_id, family, attribute, rest) as (
-    select product_id, family, (rest).key, jsonb_each((rest).value)
+  by_attr (product_id, attribute, rest) as (
+    select product_id, (rest).key, jsonb_each((rest).value)
     from by_raw_value
   ),
-  by_channel (product_id, family, attribute, channel, rest) as (
-    select product_id, family, attribute, nullif((rest).key, '<all_channels>'), jsonb_each((rest).value)
+  by_channel (product_id, attribute, channel, rest) as (
+    select product_id, attribute, nullif((rest).key, '<all_channels>'), jsonb_each((rest).value)
     from by_attr
   ),
-  by_locale (product_id, family, attribute, channel, locale, value) as (
-    select product_id, family, attribute, channel, nullif((rest).key, '<all_locales>'), (rest).value
+  by_locale (product_id, attribute, channel, locale, value) as (
+    select product_id, attribute, channel, nullif((rest).key, '<all_locales>'), (rest).value
     from by_channel
   )
   select * from by_locale
 ;
 
-create sink product_stats as select coalesce(family, 0) family, count(*) as num_values from pim1.product_value group by family
+create sink product_stats as
+select coalesce(p.family_id, 0) family, count(*) as num_values
+from pim1.product_value pv
+inner join pim1.product p on (pv.product_id = p.id)
+group by 1
 with (
     connector='jdbc',
     jdbc.url='jdbc:mysql://mysql:3306/pim1?user=root&password=root',
