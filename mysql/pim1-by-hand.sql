c414c23d9ec705b8ce8feabdc5abcb83b9778965
Florian Klein <florian.klein@free.fr>
2024-01-22 10:58:59 +0100

    agg model values

--- mysql/pim1-by-hand.sql
+++ mysql/pim1-by-hand.sql
@@ -13,15 +13,15 @@ create table pim_catalog_product (
   primary key (uuid)
 );
 
-insert into pim_catalog_product (uuid, id, family_id, raw_values) values
-(UUID_TO_BIN(uuid()), 1, 1, '{
+insert into pim_catalog_product (uuid, id, product_model_id, family_id, raw_values) values
+(UUID_TO_BIN(uuid()), 1, 4, 1, '{
     "a1": {
         "<all_channels>": {
             "<all_locales>": "p1 a1 1"
         }
     }
 }'),
-(UUID_TO_BIN(uuid()), 2, 1, '{
+(UUID_TO_BIN(uuid()), 2, 3, 1, '{
     "a1": {
         "<all_channels>": {
             "<all_locales>": "p2 a1 1"
@@ -31,6 +31,47 @@ insert into pim_catalog_product (uuid, id, family_id, raw_values) values
         "<all_channels>": {
             "en_US": "p2 a2 us 1",
             "fr_FR": "p2 a2 fr 1"
+        }
+    }
+}'),
+(UUID_TO_BIN(uuid()), 3, null, 1, '{
+    "a1": {
+        "<all_channels>": {
+            "<all_locales>": "p3 a1 1"
+        }
+    },
+    "a2": {
+        "<all_channels>": {
+            "en_US": "p3 a2 us 1",
+            "fr_FR": "p3 a2 fr 1"
+        }
+    }
+}');
+
+drop table if exists pim_catalog_product_model;
+create table pim_catalog_product_model (
+  id int,
+  code text,
+  parent_id int,
+  family_variant_id int,
+  raw_values json,
+  created timestamp,
+  updated timestamp,
+  primary key (id)
+);
+
+insert into pim_catalog_product_model (id, parent_id, family_variant_id, raw_values) values
+(3, null, 2, '{
+    "a3": {
+        "<all_channels>": {
+            "<all_locales>": "pm3 a3 1"
+        }
+    }
+}'),
+(4, 3, 2, '{
+    "a4": {
+        "<all_channels>": {
+            "<all_locales>": "pm4 a4 1"
         }
     }
 }');
--- rw/pubsub.sql
+++ rw/pubsub.sql
@@ -1,8 +1,12 @@
 drop sink if exists product_to_es;
-drop table if exists dbz_cdc;
+drop view if exists es_product;
+drop table if exists product;
+drop table if exists product_model;
 
-create table dbz_cdc (
+create table product (
     id int,
+    tenant text,
+    product_model_id int,
     raw_values jsonb,
     primary key (id)
 ) with (
@@ -13,8 +17,30 @@ create table dbz_cdc (
     pubsub.split_count = 1
 ) format debezium encode json;
 
+create table product_model (
+    id int,
+    tenant text,
+    parent_id int,
+    raw_values jsonb,
+    primary key (id)
+) with (
+    connector = 'google_pubsub',
+    pubsub.subscription = 'projects/my-project-id/subscriptions/all_pims.pim_catalog_product_model',
+    -- pubsub.credentials = '{}',
+    pubsub.emulator_host = 'pubsub-emulator:8085',
+    pubsub.split_count = 1
+) format debezium encode json;
+
+create view es_product as
+select p.id,
+coalesce(ppm.raw_values, '{}') || coalesce(pm.raw_values, '{}') || coalesce(p.raw_values, '{}') raw_values
+from product p
+left join product_model pm on (p.product_model_id = pm.id)
+left join product_model ppm on (pm.parent_id = ppm.id);
+-- group by 1, 2;
+
 create sink product_to_es
-from dbz_cdc
+from es_product
 with (
   connector = 'elasticsearch',
   index = 'product',
