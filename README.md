fb9b28010215ba6ee3a57a2afea0f80c39847291
Florian Klein <florian.klein@free.fr>
2024-01-30 09:34:52 +0100

    wip

--- README.md
+++ README.md
@@ -15,7 +15,8 @@ SQL
 #dc exec redpanda rpk topic consume product_value_edited
 
 export CLOUDSDK_API_ENDPOINT_OVERRIDES_PUBSUB=http://localhost:8085/
-gcloud pubsub --project my-project-id subscriptions create --topic all_pims.pim_catalog_attribute_translation all_pims.pim_catalog_attribute_translation  --retain-acked-messages
-gcloud pubsub --project my-project-id subscriptions create --topic all_pims.pim_catalog_attribute all_pims.pim_catalog_attribute --retain-acked-messages
+gcloud pubsub --project my-project-id topics create  all_pims.pim_catalog_product
+gcloud pubsub --project my-project-id topics create  all_pims.pim_catalog_product_model
 gcloud pubsub --project my-project-id subscriptions create --topic all_pims.pim_catalog_product all_pims.pim_catalog_product --retain-acked-messages
+gcloud pubsub --project my-project-id subscriptions create --topic all_pims.pim_catalog_product_model all_pims.pim_catalog_product_model --retain-acked-messages
 ```
--- rw/pubsub.sql
+++ rw/pubsub.sql
@@ -4,31 +4,37 @@ drop table if exists product;
 drop table if exists product_model;
 
 create table product (
-    id int,
     tenant text,
+    id int,
     product_model_id int,
     raw_values jsonb,
-    primary key (id)
+    primary key (tenant, id)
 ) with (
-    connector = 'google_pubsub',
-    pubsub.subscription = 'projects/my-project-id/subscriptions/all_pims.pim_catalog_product',
+    connector = 'kafka',
+    topic = 'all_pims.pim_catalog_product',
+    properties.bootstrap.server = 'redpanda:9092'
+    -- connector = 'google_pubsub',
+    -- pubsub.subscription = 'projects/my-project-id/subscriptions/all_pims.pim_catalog_product',
     -- pubsub.credentials = '{}',
-    pubsub.emulator_host = 'pubsub-emulator:8085',
-    pubsub.split_count = 1
+    -- pubsub.emulator_host = 'pubsub-emulator:8085',
+    -- pubsub.split_count = 1
 ) format debezium encode json;
 
 create table product_model (
-    id int,
     tenant text,
+    id int,
     parent_id int,
     raw_values jsonb,
-    primary key (id)
+    primary key (tenant, id)
 ) with (
-    connector = 'google_pubsub',
-    pubsub.subscription = 'projects/my-project-id/subscriptions/all_pims.pim_catalog_product_model',
+    connector = 'kafka',
+    topic = 'all_pims.pim_catalog_product_model',
+    properties.bootstrap.server = 'redpanda:9092'
+    -- connector = 'google_pubsub',
+    -- pubsub.subscription = 'projects/my-project-id/subscriptions/all_pims.pim_catalog_product_model',
     -- pubsub.credentials = '{}',
-    pubsub.emulator_host = 'pubsub-emulator:8085',
-    pubsub.split_count = 1
+    -- pubsub.emulator_host = 'pubsub-emulator:8085',
+    -- pubsub.split_count = 1
 ) format debezium encode json;
 
 create view es_product as
@@ -37,7 +43,6 @@ coalesce(ppm.raw_values, '{}') || coalesce(pm.raw_values, '{}') || coalesce(p.ra
 from product p
 left join product_model pm on (p.product_model_id = pm.id)
 left join product_model ppm on (pm.parent_id = ppm.id);
--- group by 1, 2;
 
 create sink product_to_es
 from es_product
